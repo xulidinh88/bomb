edb18ea6992f52c11d3329fcf89751db990169ab
XuanLinh <Xulidinh88@gmail.com>
2020-12-02 23:43:19 +0700

    Edit model, add controller and Animation

--- src/sample/Model/Flame.java
+++
@@ -1,7 +0,0 @@
-package sample.Model;
-
-public class Flame extends Entity{
-    public Flame(int x, int y, String imgpath, int width, int height) {
-        super(x, y, imgpath, width, height);
-    }
-}
